e589b78d0d36dd3d03915bd8f9eae6cb7371e2cb
omarallabadi <omarallabadi197@gmail.com>
2025-02-15 11:14:14 +0300

    tailwind instalation

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./App.css";
+const App = () => {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+    Hello, Tailwind CSS is working!
+  </div>
+  )
+}
+
+export default App
